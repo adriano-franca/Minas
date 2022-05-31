9aaf9d20934c6f4b923c339308e388d3f9f24cd1
adrianofranca111 <adriano.franca@discente.ufma.br>
2022-05-31 10:03:43 -0300

    refazendo o primeiro commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, SafeAreaView } from 'react-native'
+import React, { Component } from 'react'
+
+import params from './src/params'
+
+export default class App extends Component {
+  render(){
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.welcome}>Iniciando o Mines</Text>
+      <Text style={styles.instructions}>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    fontSize: 15,
+    }
+  }
+);
