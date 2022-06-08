8dc488cd18a9710bd2c31696727a518deb9672e3
adrianofranca111 <adriano.franca@discente.ufma.br>
2022-06-08 11:08:57 -0300

    criando campo

--- App.js
+++ App.js
@@ -3,27 +3,38 @@ import { StyleSheet, Text, SafeAreaView } from 'react-native'
 import React, { Component } from 'react'
 
 import params from './src/params'
-import Field from './src/components/Field'
+import MineField from './src/components/MineField'
+import { createMinedBoard } from './src/functions'
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols*rows*params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: this.createMinedBoard(rows, cols, this.minesAmount())
+    }
+  }
+
   render(){
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.welcome}>Iniciando o Mines</Text>
       <Text style={styles.instructions}>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
-      <Field />
-      <Field opened />
-      <Field opened nearMines={1} />
-      <Field opened nearMines={2} />
-      <Field opened nearMines={3} />
-      <Field opened nearMines={4} />
-      <Field opened nearMines={5} />
-      <Field opened nearMines={6} />
-      <Field mined />
-      <Field mined opened />
-      <Field mined opened exploded />
-      <Field flagged />
-      <Field flagged opened />
+        <SafeAreaView style={styles.board}>
+          <MineField board={this.state.board} />
+        </SafeAreaView>
       <StatusBar style="auto" />
     </SafeAreaView>
     )
@@ -33,17 +44,11 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'flex-end',
+  },
+  board: {
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    fontSize: 15,
-    }
+    backgroundColor: '#AAA'
+  }
   }
 );
---
+++ src/components/MineField.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { SafeAreaView, View, StyleSheet } from 'react-native'
+import Field from './Field'
+
+export default props => {
+    const rows = props.board.map((row, r) => {
+        const columns = row.map((field, c) => {
+            return <Field {...field} key={c} />
+        })
+        return <SafeAreaView key={r}>{columns}</SafeAreaView>
+    })
+    return <SafeAreaView style={styles.container}>{rows}</SafeAreaView>
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        backgroundColor: '#EEE'
+    }
+})
